f5991f642a8721746762f1343e7046b8ce4ccadd
Rohit <rohitjadhav1612@gmail.com>
2024-07-01 08:33:01 +0530

    Added Login and Register Page code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import './Page.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="br-rds col-xs-12 col-sm-8 col-md-6 col-lg-5">
+          <div class="card">
+            <div class="card-body my-card">
+              <div class="card-header">
+                <h5 class="text-center">SIGN IN</h5>
+              </div>
+
+              <div class="d-flex justify-content-center my-4">
+                <i class="fa-solid fa-circle-user fa-5x"></i>
+              </div>
+              <form action="">
+                <div class="form-group mt-3 position-relative">
+                  <i class="fa-solid fa-user fa-xl my-user"></i>
+                  <input type="text" class="form-control ps-5" placeholder="username" />
+                </div>
+                <div class="form-group mt-3 position-relative">
+                  <i class="fa-solid fa-lock fa-xl my-user"></i>
+                  <input type="password" class="form-control ps-5" placeholder="password" />
+                </div>
+                <button class="btn btn-outline-success form-control mt-4">LOGIN</button>
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,39 @@ import React from 'react'
 const Register = () => {
     return (
         <>
-            {/* Registration Page */}
+            <div class="main"></div>
+            <div class="container">
+                <div class="row justify-content-center mt-5">
+                    <div class="col-xs-12 col-sm-8 col-md-6 col-lg-5">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">Register</h5>
+                            </div>
+                            <div class="card-body">
+                                <form action="">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" class="form-control" id="name" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="date">Date</label>
+                                        <input type="date" class="form-control" id="date" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="password" />
+                                    </div>
+                                    <button class="btn btn-outline-success form-control mt-3">Register</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
